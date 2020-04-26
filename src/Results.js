9f4b5df108767224d3d49bade445273188586ec4
JeffML <jlowery2663@gmail.com>
2020-04-26 13:07:23 -0700

    fixes?

--- src/Results.js
+++ src/Results.js
@@ -1,78 +1,101 @@
 /* global BigInt */
-import React from 'react';
+import React from "react";
 
-const color = (row) => row % 2 ? '#C3C4C5' : "#F5F3F3"
+const color = row => (row % 2 ? "#C3C4C5" : "#F5F3F3");
 
-const nanoToSec = (time) => {
-  var pTime = time.padStart(10, 0)
-  var dec = pTime.slice(-9)
-  var sec = pTime.slice(0, pTime.length - 9)
-  var fTime = parseFloat(`${sec}.${dec}`).toFixed(9)
+const nanoToSec = time => {
+  var pTime = time.padStart(10, 0);
+  var dec = pTime.slice(-9);
+  var sec = pTime.slice(0, pTime.length - 9);
+  var fTime = parseFloat(`${sec}.${dec}`).toFixed(9);
   return fTime;
-}
+};
 
-const ZERO = BigInt(0)
+const ZERO = BigInt(0);
 
 function filterDupes(primes) {
-  return primes.reduce((acc, r) => {
-    const lastPrime = acc.uniqPrimes.length && acc.uniqPrimes.slice(-1)[0].prime;
-    if (r.prime !== lastPrime) {
-      acc.sumOfPrimeTimes += BigInt(r.time);
-      acc.uniqPrimes.push(r);
-    }
-    return acc;
-  }, { sumOfPrimeTimes: ZERO, uniqPrimes: [] });
+  return primes.reduce(
+    (acc, r) => {
+      const lastPrime =
+        acc.uniqPrimes.length && acc.uniqPrimes.slice(-1)[0].prime;
+      if (r.prime !== lastPrime) {
+        acc.sumOfPrimeTimes += BigInt(r.time);
+        acc.uniqPrimes.push(r);
+      }
+      return acc;
+    },
+    { sumOfPrimeTimes: ZERO, uniqPrimes: [] }
+  );
 }
 
 const Responses = ({ primes }) => {
-  console.log("primes>>>>>", primes)
   return primes.map((result, i) => {
     const { prime, time, factors } = result;
-    const falsePrime = factors && factors.includes('*')
+    const falsePrime = factors && factors.includes("*");
 
     if (time) {
-      var fTime = nanoToSec(time)
+      var fTime = nanoToSec(time);
     }
-    return <div className="row" style={{ maxHeight: '1.2rem', color: falsePrime ? 'red' : 'black', backgroundColor: color(i) }} key={prime}>
-      <div className="column">
-        <span>Prime: {prime}</span><br></br>
-      </div>
-      <div className="column">
-        <span>Time: {fTime}</span>
-      </div>
-      {factors ?
+    return (
+      <div
+        className="row"
+        style={{
+          maxHeight: "1.2rem",
+          color: falsePrime ? "red" : "black",
+          backgroundColor: color(i)
+        }}
+        key={prime}
+      >
         <div className="column">
-          <span>Factors: {factors}</span>
-        </div> : null}
-    </div>
+          <span>Prime: {prime}</span>
+          <br></br>
+        </div>
+        <div className="column">
+          <span>Time: {fTime}</span>
+        </div>
+        {factors ? (
+          <div className="column">
+            <span>Factors: {factors}</span>
+          </div>
+        ) : null}
+      </div>
+    );
   });
-}
+};
 
 export default ({ results, method }) => {
-  console.log({ results, method })
+  console.log({ results, method });
   const { totalTime, responses } = results;
-  
-  if (method === 'old' && Array.isArray(responses)) {
-    var { sumOfPrimeTimes, uniqPrimes } = filterDupes(responses)
+
+  if (method === "old" && Array.isArray(responses)) {
+    var { sumOfPrimeTimes, uniqPrimes } = filterDupes(responses);
   } else if (responses.primes) {
     sumOfPrimeTimes = totalTime;
     const factors = responses.factors;
-    uniqPrimes = (responses.primes || []).map((prime,i) => ({prime, factors: factors && factors[i]}));
+    uniqPrimes = (responses.primes || []).map((prime, i) => ({
+      prime,
+      factors: factors && factors[i]
+    }));
   } else {
     return null;
   }
 
-  return <div>
-    <div className="row">
-      <div className="column">
-        <span style={{ fontWeight: 'bold' }}>Total run time: {totalTime && nanoToSec(totalTime)}</span>
+  return (
+    <div>
+      <div className="row">
+        <div className="column">
+          <span style={{ fontWeight: "bold" }}>
+            Total run time: {totalTime && nanoToSec(totalTime)}
+          </span>
+        </div>
+        <div className="column">
+          <span style={{ fontWeight: "bold" }}>
+            Sum of prime times:{" "}
+            {sumOfPrimeTimes && nanoToSec(sumOfPrimeTimes.toString())}
+          </span>
+        </div>
       </div>
-      <div className="column">
-        <span style={{ fontWeight: 'bold' }}>Sum of prime times: {sumOfPrimeTimes && nanoToSec(sumOfPrimeTimes.toString())}</span>
-      </div>
+      <Responses {...{ primes: uniqPrimes }} />
     </div>
-    <Responses {...{ primes: uniqPrimes }} />
-  </div>
+  );
 };
-
-
